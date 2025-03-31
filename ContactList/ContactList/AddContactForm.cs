2cf094943ded9dbc4da00b42815239b30626a8b8
Kien <ntkbmt3@gmail,com>
2025-03-31 14:15:23 +0700

    Thêm form để thêm liên hệ mới

--- ContactList/ContactList/AddContactForm.cs
+++ ContactList/ContactList/AddContactForm.cs
@@ -10,14 +10,14 @@ using System.Windows.Forms;
 
 namespace ContactList
 {
-    public partial class MainForm : Form
+    public partial class AddContactForm : Form
     {
-        public MainForm()
+        public AddContactForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
